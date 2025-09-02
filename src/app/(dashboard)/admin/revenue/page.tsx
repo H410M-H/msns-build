cf0d8fa516828cdd147419131a64813300310ccc
HH_STUDIOS <46568247+H410M-H@users.noreply.github.com>
2025-09-02 14:03:09 +0500

    Update page.tsx

--- src/app/(dashboard)/admin/revenue/page.tsx
+++ src/app/(dashboard)/admin/revenue/page.tsx
@@ -110,7 +110,7 @@ export default function RevenuePage() {
             <RevenueCards />
           </div>
        <div className="p-4 md:p-6">
-            <ExpansesTable />
+            <ExpensesTable />
           </div>
         </motion.div>
       </CardContent>
