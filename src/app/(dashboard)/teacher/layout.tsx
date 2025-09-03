127f597d0abdecd8a5a6f56b88afa426a5bf8775
H410M-H <hassaanazam7@gmail.com>
2025-09-04 01:35:02 +0500

    UIEnchance

---
+++ src/app/(dashboard)/teacher/layout.tsx
@@ -0,0 +1,22 @@
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "~/components/ui/sidebar";
+import { AppSidebar } from "~/components/blocks/sidebar/app-sidebar";
+
+export default async function TeacherDashboardLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <>
+      <SidebarProvider>
+        <AppSidebar className="h-screen" />
+        <SidebarInset>
+          <main className="w-full h-screen bg-from-yellow-100/20 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+            {children}
+          </main>
+        </SidebarInset>
+      </SidebarProvider>
+    </>
+  );
+}
