95a44c8bbbbb6f76064e171df4861932884aaecd
h410m-h <hassaanazam7@gmsil.com>
2026-08-22 13:30:47 +0500

    fix(android): resolve FirebaseApp initialization and theme splashscreen crashes

--- android/app/src/main/java/com/msns/lms/MainActivity.java
+++ android/app/src/main/java/com/msns/lms/MainActivity.java
@@ -1,5 +1,25 @@
 package com.msns.lms;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            if (FirebaseApp.getApps(this).isEmpty()) {
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setApplicationId("1:123456789012:android:msns")
+                        .setProjectId("msns-lms")
+                        .setApiKey("AIzaSyDummyApiKeyForMSNSLMSAppStartup")
+                        .build();
+                FirebaseApp.initializeApp(this, options);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
